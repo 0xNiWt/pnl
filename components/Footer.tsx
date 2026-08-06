2444d04b72d0359dc3761c1a1844bbe0ed454497
0xNiWt <niwtbwy@gmail.com>
2026-08-07 02:33:33 +0300

    first commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
             <div className="flex items-center justify-center flex-col gap-2">
                 <p className="text-primary text-sx">&copy; {new Date().getFullYear()} Природничо-науковий ліцей №145. Всі права захищені.</p>
                 <p className="text-gray-400 text-sm">Розроблено GhostStudio</p>
-                {/* <a href="https://ghoststudio.online" className="text-blue-500 hover:underline">GhostStudio</a> */}
             </div>
         </footer>
     )
