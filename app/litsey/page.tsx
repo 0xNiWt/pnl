cbd3ebcf1fa7001df7d7786a361571a2572ba958
raftelgo981-pixel <raftelgo981@gmail.com>
2026-08-11 13:53:35 +0300

    zzzzzz

--- app/litsey/page.tsx
+++ app/litsey/page.tsx
@@ -4,6 +4,7 @@ import Footer from "@/components/Footer";
 export default function RegisterPage() {
     return (
         <main className="bg-background min-h-screen flex flex-col">
+            <div><h2>zzzzzzz</h2></div>
             <Contact />
             <Footer />
         </main>
