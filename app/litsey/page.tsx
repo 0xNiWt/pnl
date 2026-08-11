28a97fc74da713675a0dc32ea4a5784dfefbf544
raftelgo981-pixel <raftelgo981@gmail.com>
2026-08-11 13:50:01 +0300

    Nu sasha tipo ja ga.... oy ya krutoy

--- app/litsey/page.tsx
+++ app/litsey/page.tsx
@@ -4,7 +4,8 @@ import Footer from "@/components/Footer";
 export default function RegisterPage() {
     return (
         <main className="bg-background min-h-screen flex flex-col">
-            <Contact />
+           <h2>ZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZZCVOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO</h2>
+          <Contact />
             <Footer />
         </main>
     )
