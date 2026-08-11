dba6a0a96e327dc2ab4f7ee6b788a419913610e3
0xNiWt <4sfvckgn56@privaterelay.appleid.com>
2026-08-11 19:44:29 +0300

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     return (
         <header className="sticky top-0 z-50 bg-background px-4 py-4 border-b border-gray-800/20">
             <div className="md:max-w-7xl mx-auto">
-                <div className="flex items-center justify-between md:gap-12">
+                <div className="flex items-center justify-between md:gap-12 flex-wrap">
                     <Link href="/" className="flex items-center gap-2 group">
                         <Image src={gerb} alt="Герб ліцею" className="h-16 w-auto" priority />
                         <h1 className="text-primary text-[clamp(1rem,0.7143rem_+_0.5952vw,1.25rem)] font-inter font-bold">
